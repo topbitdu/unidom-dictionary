6d7364fe17d0ef2e426490f4bfe8657d045e7fb5
Topbit Du <topbit.du@gmail.com>
2016-12-06 09:06:55 +0800

    1, Dictionary Item model & migration.

---
+++ app/models/unidom/dictionary/dictionary_item.rb
@@ -0,0 +1,11 @@
+# Dictionary Item 是字典项。
+
+class Unidom::Dictionary::DictionaryItem < Unidom::Dictionary::ApplicationRecord
+
+  self.table_name = 'unidom_dictionary_items'
+
+  include Unidom::Common::Concerns::ModelExtension
+
+  validates :value, presence: true, length: { in: 2..self.columns_hash['value'].limit }
+
+end
---
+++ db/migrate/20000701000000_create_unidom_dictionary_items.rb
@@ -0,0 +1,26 @@
+class CreateUnidomDictionaryItems < ActiveRecord::Migration
+
+  def change
+
+    create_table :unidom_dictionary_items, id: :uuid do |t|
+
+      t.column :dictionary_code, 'char(4)', null: false, default: 'C'
+
+      t.string :value, null: false, default: '', limit: 200
+
+      t.text :description
+      t.text :instruction
+
+      t.column   :state, 'char(1)', null: false, default: 'C'
+      t.datetime :opened_at,        null: false, default: Time.utc(1970)
+      t.datetime :closed_at,        null: false, default: Time.utc(3000)
+      t.boolean  :defunct,          null: false, default: false
+      t.jsonb    :notation,         null: false, default: {}
+
+      t.timestamps null: false
+
+    end
+
+  end
+
+end
