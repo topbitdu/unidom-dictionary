368cd355b01886bd7414697fb7efd60c2c8d27cb
Topbit Du <topbit.du@gmail.com>
2017-01-18 12:00:28 +0800

    1, Improve the Dictionary Item model to support the namespace neglecting.

--- app/models/unidom/dictionary/dictionary_item.rb
+++ app/models/unidom/dictionary/dictionary_item.rb
@@ -10,4 +10,4 @@ class Unidom::Dictionary::DictionaryItem < Unidom::Dictionary::ApplicationRecord
   validates :code,  allow_blank: true, length: { in: 1..self.columns_hash['code'].limit  }
   validates :value, presence:    true, length: { in: 2..self.columns_hash['value'].limit }
 
-end
+end unless Unidom::Common::Neglection.namespace_neglected? 'Unidom::Dictionary::DictionaryItem'
