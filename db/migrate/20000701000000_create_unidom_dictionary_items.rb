56e977f02b7e9afde21cbf91b28a2730f27cbda9
Topbit Du <topbit.du@gmail.com>
2017-01-29 10:31:12 +0800

    1, Improve the Dictionary Item migration for the default value of the #dictionary_code column.

--- db/migrate/20000701000000_create_unidom_dictionary_items.rb
+++ db/migrate/20000701000000_create_unidom_dictionary_items.rb
@@ -4,7 +4,7 @@ class CreateUnidomDictionaryItems < ActiveRecord::Migration
 
     create_table :unidom_dictionary_items, id: :uuid do |t|
 
-      t.column :dictionary_code, 'char(4)', null: false, default: 'C'
+      t.column :dictionary_code, 'char(4)', null: false, default: 'ZZZZ'
       t.string :code,                       null: true,  default: nil, limit: 64
 
       t.string :value, null: false, default: '', limit: 200
