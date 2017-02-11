b622fbc47616fc5a896f9d51b612aded5524fd4a
Topbit Du <topbit.du@gmail.com>
2017-02-11 09:41:57 +0800

    1, Improve the Dictionary Item model to remove the validations on the #code attribute.

--- app/models/unidom/dictionary/dictionary_item.rb
+++ app/models/unidom/dictionary/dictionary_item.rb
@@ -7,7 +7,6 @@ class Unidom::Dictionary::DictionaryItem < Unidom::Dictionary::ApplicationRecord
 
   include Unidom::Common::Concerns::ModelExtension
 
-  validates :code,  allow_blank: true, length: { in: 1..self.columns_hash['code'].limit  }
-  validates :value, presence:    true, length: { in: 2..self.columns_hash['value'].limit }
+  validates :value, presence: true, length: { in: 2..self.columns_hash['value'].limit }
 
 end unless Unidom::Common::Neglection.namespace_neglected? 'Unidom::Dictionary::DictionaryItem'
